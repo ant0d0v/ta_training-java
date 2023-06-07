022aad3eb268fda64a3eec2214512c523fed48e8
ant0d0v <gosu1203@gmail.com>
2023-06-07 17:34:13 +0300

    first commit

---
+++ src/main/java/com/epam/training/student_Anton_Udovychenko/fundamental/Main.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_Anton_Udovychenko.fundamental;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/epam/training/student_Anton_Udovychenko/fundamental/main_task/HelloUser.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_Anton_Udovychenko.fundamental.main_task;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
